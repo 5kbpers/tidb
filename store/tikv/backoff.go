2c4d716a6e96c6c74ee11bd5e0f78730a8c1a7df
5kbpers <tangminghua@pingcap.com>
2020-09-22 20:24:13 +0800

    region_cache: change parameters of pd backoff
    Signed-off-by: 5kbpers <tangminghua@pingcap.com>

--- store/tikv/backoff.go
+++ store/tikv/backoff.go
@@ -151,7 +151,7 @@ func (t backoffType) createFn(vars *kv.Variables) func(context.Context, int) int
 	case boTxnLockFast:
 		return NewBackoffFn(vars.BackoffLockFast, 3000, EqualJitter)
 	case BoPDRPC:
-		return NewBackoffFn(500, 3000, EqualJitter)
+		return NewBackoffFn(50, 500, EqualJitter)
 	case BoRegionMiss:
 		// change base time to 2ms, because it may recover soon.
 		return NewBackoffFn(2, 500, NoJitter)
